65f4844336576541a3dc2404608265709125fbaf
Jatin1o1 <r.jatin445@gmail.com>
2021-01-21 17:09:29 +0530

    mqtt publisher funciton added

--- includes/mqtt_pub.py
+++ includes/mqtt_pub.py
@@ -1,1 +1,18 @@
-pass
+
+import paho.mqtt.client as mqtt
+
+host = "127.0.0.1"
+
+
+client = mqtt.Client("P1")  # create new instance
+client.connect(host)
+
+# function to  publish at mqtt topic Mqtt_publish(topic,message)
+def Mqtt_publish(topic,message):   # function to  publish at mqtt topic Mqtt_publish(topic,message)
+    client.connect(host)
+    client.publish(topic, message)
+    client.disconnect()
+
+
+if __name__ == "__main__":
+    Mqtt_publish("test", "Hi it is a message")  # topic name is test, messge is "hi it is a message"
